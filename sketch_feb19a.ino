11457f01f832267f23b4d708708e1b8a8f8b0c9d
Aaron Jumarang <kapj.jumarang@gmail.com>
2022-02-21 13:45:36 -0500

    Added Width for Distance Measurement

--- sketch_feb19a.ino
+++ sketch_feb19a.ino
@@ -4,37 +4,44 @@ Pixy2 pixy;
 int x_pos;
 int y_pos;
 String output;
+int colour; //R  or   B
+
+//Sig 1 = red
+//Sig 2 = blue
+
 
 void setup() {
   Serial.begin(115200);
   Wire.begin(4);
   Wire.onRequest(requestEvent); // Register a function to be called when a master requests data from this slave device
   pixy.init();
-  output = "none";
+  output = "";
 }
 
 void loop() {
   pixy.ccc.getBlocks();
   double area = 0, temp;
   int biggest;
-  if (pixy.ccc.numBlocks)
-  {
+  if (pixy.ccc.numBlocks) { 
     for (int i=0; i<pixy.ccc.numBlocks; i++) {
-      temp = pixy.ccc.blocks[i].m_y;
-      if(temp > area){
-        area = temp;
-        biggest = i;
+      if(pixy.ccc.blocks[i].m_signature == colour){
+        temp = pixy.ccc.blocks[i].m_width * pixy.ccc.blocks[i].m_height;
+        if(temp > area){
+          area = temp;
+          biggest = i;
+        }
       }
+      output = String(pixy.ccc.blocks[biggest].m_x);
+      output += "|";
+      output += String(pixy.ccc.blocks[biggest].m_width);
     }
-    x_pos = pixy.ccc.blocks[biggest].m_x;
-    y_pos = pixy.ccc.blocks[biggest].m_y;
+  }
+  else{
+    output = "none";
+  }
 
-
-    output = x_pos;
-
-    delay(70); //gives time for everything to process
-    Serial.println(output);
-  }  
+  delay(70); //gives time for everything to process
+  Serial.println(output);  
 }
 
 void requestEvent(){//called when RoboRIO request a message from this device
